45591c52844ce2e072ee14d10da29ed023fdf895
Denissevar <dvargasvel@gmail.com>
2025-04-22 09:41:17 +0200

    ultimo cambio

--- dom/app.js
+++ dom/app.js
@@ -1,17 +1,17 @@
 // ¿En tu páis de origen es normal dejar propina?, en Colombia no es obligatorio pero si es esperado dejar algo de propina.
 
 document.querySelector(`#calculate`).addEventListener(`click`, function () {
-const bill = parseFloat(document.querySelector("#bill-amount").value);
-const percentage = parseFloat(document.querySelector("#percentage-tip").value);
+    const bill = parseFloat(document.querySelector("#bill-amount").value);
+    const percentage = parseFloat(document.querySelector("#percentage-tip").value);
 
-if (isNaN(bill) || isNaN(percentage)) {
-    alert(`Por favor, introduce numeros validos en ambos campos. `);
-    return;
-}
+    if (isNaN(bill) || isNaN(percentage)) {
+        alert(`Por favor, introduce numeros validos en ambos campos. `);
+        return;
+    }
 
-const tip = bill * (percentage / 100);
-const total = bill + tip;
+    const tip = bill * (percentage / 100);
+    const total = bill + tip;
 
-document.querySelector(`#tip-amount`).value = tip.toFixed(2);
-document.querySelector(`#total`).value = total.toFixed(2);
+    document.querySelector(`#tip-amount`).value = tip.toFixed(2);
+    document.querySelector(`#total`).value = total.toFixed(2);
 });
